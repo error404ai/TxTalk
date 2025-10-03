b1d0b3ac616b87470e13e23d3e1c55eeb31f9a10
skmirajbn <skmirajbn@gmail.com>
2025-10-04 00:43:50 +0600

    Update navigation links and add NotFound component

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -7,7 +7,7 @@ const RootLayout = () => (
       <Link to="/" className="[&.active]:font-bold">
         Home
       </Link>{" "}
-      <Link to="/" className="[&.active]:font-bold">
+      <Link to="/about" className="[&.active]:font-bold">
         About
       </Link>
     </div>
@@ -17,4 +17,17 @@ const RootLayout = () => (
   </>
 );
 
-export const Route = createRootRoute({ component: RootLayout });
+const NotFound = () => (
+  <div className="p-4 space-y-2">
+    <h1 className="text-xl font-semibold">Page not found</h1>
+    <p>We couldn't find that page. Use the navigation above to get back on track.</p>
+    <Link to="/" className="text-blue-600 hover:underline">
+      Go home
+    </Link>
+  </div>
+);
+
+export const Route = createRootRoute({
+  component: RootLayout,
+  notFoundComponent: NotFound,
+});
